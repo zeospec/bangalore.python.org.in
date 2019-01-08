d15584687e581ab83f5e765452ec4fb9a3e17935
Arun Teja Godavarthi <www.aruntejathefriend@gmail.com>
2019-01-08 22:41:08 +0530

    adding environment variable

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ev && \
+set -e && \
 cd ./_site && \
 remote_branch="gh-pages" && \
 git init && \
@@ -7,6 +7,6 @@ git config user.name "Travis-CI" && \
 git config user.email "contact@travis-ci.com" && \
 git add . && \
 git commit -m'Deployed to Github' && \
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1 && \
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:$remote_branch > /dev/null 2>&1 && \
 rm -fr .git && \
 cd ../
